537a1abdb47acd0ddd579ed3e51fa177ee462a3c
neex-io <neex.io@protonmail.com>
2017-12-24 09:13:15 -0800

    Add get_cipher_choice function

--- secret_message.py
+++ secret_message.py
@@ -1,3 +1,35 @@
+import os
+
+def clear():
+    """Clears console."""
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def display_menu():
+    """Displays manu of ciphers available for encoding messages"""
+
+    menu = """Welcome! If you have a secret you are in a good place.
+Here is a set of great tools to keep you message from unwanted attention:
+1. Affine (A)
+2. Caesar (C)
+3. -
+4. -
+"""
+    return menu
+
+def get_cipher_choice():
+    """Validates the user's cipher choice against the available ciphers"""
+    
+    cipher_code_letters = ["A", "C"] 
+    cipher_choice = ""
+
+    while cipher_choice.upper() not in cipher_code_letters:
+        cipher_choice = input("Please pick corresponding letter to choose "\
+                              "cipher\n>>> ").upper()
+        if cipher_choice in cipher_code_letters: # if all OK, return
+            return cipher_choice
+        # TODO: add a Q for quitting the entire program with sys module
+        print("Errrr, something went wrong.") # else, try again
+        display_menu()
 
 def encryption_runner():
     """Asks user for choice of cypher and required data.
@@ -6,9 +38,9 @@ def encryption_runner():
 
     # format display nicely
     clear() # clear screen
-    display_menu() # display menu choices
+    print(display_menu()) # display menu choices
     cipher = get_cipher_choice() # inf. loop to get correct cypher code
-    clear() # clears out
+    # clear() # clears out
     
     # get from user if it is D or E
     # get the message to D or E
@@ -16,24 +48,27 @@ def encryption_runner():
     # print out the E/D message
     # ask for rematch :) ?
 
-    encode_decode = input(
-        "I see, you want to use {}.\
-        Are we encoding (E) or decoding (D) a message?".format(cipher))
+    # encode_decode = input(
+    #     "I see, you want to use {}.\
+    #     Are we encoding (E) or decoding (D) a message?".format(cipher))
     
-    message = input(
-        "Got it! I see, you want to use {}.\
-        Are we encoding (E) or decoding (D) a message?".format(cipher))
+    # message = input(
+    #     "Got it! I see, you want to use {}.\
+    #     Are we encoding (E) or decoding (D) a message?".format(cipher))
     
 
-    encode_message = 
+    # encode_message = 
 
-    encode_choice = validate_encode_choice()
-    message_code = run_cipher(cipher_choice, message, encode_choice)
-    print(message_code.upper())    
+    # encode_choice = validate_encode_choice()
+    # message_code = run_cipher(cipher_choice, message, encode_choice)
+    # print(message_code.upper())    
 
 
 
 
 if __name__ == "__main__":
-    while True:
-        pass  
+    while True: # runs always until told to stop
+        encryption_runner()
+        run_again = input("Would you like to have another go?\n>>> ").lower()
+        if run_again != 'y':
+            break
